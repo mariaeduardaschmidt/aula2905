55bfa64e8a3a393af076e0c6bc78f78735073fa0
Maria Eduarda Schmidt <166662568+mariaeduardaschmidt@users.noreply.github.com>
2024-05-29 08:55:25 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-document.getElementById(&#39;messageButton&#39;).addEventListener(&#39;click&#39;, function() {
-    var messageParagraph = document.getElementById(&#39;message&#39;);
-    if (messageParagraph.classList.contains(&#39;hidden&#39;)) {
-    messageParagraph.classList.remove(&#39;hidden&#39;);
+document.getElementById('messageButton').addEventListener('click', function() {
+    var messageParagraph = document.getElementById('message');
+    if (messageParagraph.classList.contains('hidden')) {
+        messageParagraph.classList.remove('hidden');
     } else {
-    messageParagraph.classList.add(&#39;hidden&#39;);
+        messageParagraph.classList.add('hidden');
     }
-    });
+});
